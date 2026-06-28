efc369bf4c086e41fb2cc7ada205dc75f90dc241
Arnold <arnold.gallegosm@gmail.com>
2026-06-27 21:08:26 -0500

    ⚡️Update: Guia Python Flask Email

--- 05-Flask-Email/app.py
+++ 05-Flask-Email/app.py
@@ -5,16 +5,20 @@ from flask_restful import Api
 from flask_mail import Mail
 from models import usuarios
 from resources.usuariosResource import UsuariosResource
+from dotenv import load_dotenv
+import os
 
 app = Flask(__name__)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
+load_dotenv()
+
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv('SQLALCHEMY_DATABASE_URI')
 # Configuración de Flask-Mail
-app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USERNAME'] = 'arnold.gallegosm@gmail.com'
-app.config['MAIL_PASSWORD'] = 'mwdh atnx yjdc vuid'
-app.config['MAIL_DEFAULT_SENDER'] = 'arnold.gallegosm@gmail.com'
+app.config['MAIL_SERVER'] = os.getenv('MAIL_SERVER')
+app.config['MAIL_PORT'] = os.getenv('MAIL_PORT')
+app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
+app.config['MAIL_DEFAULT_SENDER'] = os.getenv('MAIL_DEFAULT_SENDER')
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USE_SSL'] = False
 
